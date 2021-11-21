43a5471bd598ab5bb9b7477ee5ad4a41d0c7bc40
lam.nguyenvuduc <nguyenvuduclam@gmail.com>
2021-11-21 20:08:34 +0700

    change something

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public Transform target;
+    private Transform target;
+    private float turningSpeed = 10f;
     public float range = 15f;
+    public string enemyTag = "Enemy";
+    public Transform rotatePart;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("UpdateTarget",);
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void UpdateTarget(){
+        if (target == null)
+            return;
+        AimTarget();
 
     }
-    private void OnDrawGizmosSelected() {
+    private void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if (nearestEnemy != null && shortestDistance < range)
+            target = nearestEnemy.transform;
+        else target = null;
+    }
+
+    private void AimTarget()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(rotatePart.rotation, lookRotation,Time.deltaTime*turningSpeed).eulerAngles;
+        rotatePart.rotation = Quaternion.Euler(0,rotation.y,0);
+    }
+    private void OnDrawGizmosSelected()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
     }
